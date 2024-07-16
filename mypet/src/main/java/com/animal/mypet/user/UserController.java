f4e108e59af7497a674718c4225f97d34fbce09a
LastProject <tjdgus5099@naver.com>
2024-07-16 10:59:45 +0900

    [Feat] getUser메서드 추가 #15
    User 패키지에 getUser(String username) 메소드 추가
    사용자 검색을 위해 userRepository.findByUserId(username) 사용
    사용자가 존재하지 않을 경우 DataNotFoundException 예외 발생
    
    -

--- mypet/src/main/java/com/animal/mypet/user/UserController.java
+++ mypet/src/main/java/com/animal/mypet/user/UserController.java
@@ -1,7 +1,7 @@
 package com.animal.mypet.user;
 
 import java.util.HashMap;
-
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -11,8 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,14 +26,6 @@ public class UserController {
     
     private final UserService userService;
     private final UserRepository userRepository; // UserRepository 추가
-    
-//    카카오톡 로그인
-   @Value("${kakao.client_id}")
-    private String client_id;
-
-    @Value("${kakao.redirect_uri}")
-    private String redirect_uri;
-    
     
     
     @GetMapping("/signup")
@@ -84,9 +78,39 @@ public class UserController {
     
     @GetMapping("/login")
     public String login(Model model) {
-        String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
-         model.addAttribute("location", location);
         return "login/form";
+    }
+    
+    
+ // 관리자 페이지 이동
+    @GetMapping("/admin")
+    public String adminPage(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        return "admin/adminMain";
+    }
+
+    @PostMapping("/editRole/{userId}")
+    public String editUserRole(@PathVariable("userId") String userId, @RequestParam("newRole") String newRole) {
+        Optional<User> userOpt = userRepository.findByUserId(userId);
+        if (userOpt.isEmpty()) {
+            return "redirect:/user/admin";
+        }
+        User user = userOpt.get();
+        user.setUserRole(newRole);
+        userRepository.save(user);
+        return "redirect:/user/admin";
+    }
+
+    @PostMapping("/delete/{userId}")
+    public String deleteUser(@PathVariable("userId") String userId) {
+        Optional<User> userOpt = userRepository.findByUserId(userId);
+        if (userOpt.isEmpty()) {
+            return "redirect:/user/admin";
+        }
+        User user = userOpt.get();
+        userRepository.delete(user);
+        return "redirect:/user/admin";
     }
 
 }
