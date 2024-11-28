b0f75c1d7c2f9b1856af41d5ffe6e38476254823
tjdgu <tjdgus5099@naver.com>
2024-11-28 13:23:17 +0900

    session 자동 로그아웃 기능구현

--- mypet/src/main/java/com/animal/mypet/SecurityConfig.java
+++ mypet/src/main/java/com/animal/mypet/SecurityConfig.java
@@ -75,7 +75,6 @@ public class SecurityConfig {
             .exceptionHandling(exceptionHandling -> exceptionHandling
                 .accessDeniedPage("/access_denied")  // 접근 거부 페이지
             );
-        
         return http.build();
     }
 
