64e2271c2fc2d1c4fb07b95e0fef61b0b34e1f28
tjdgu <tjdgus5099@naver.com>
2024-12-15 02:04:19 +0900

    ddd

--- mypet/src/main/java/com/animal/mypet/board/file/FileService.java
+++ mypet/src/main/java/com/animal/mypet/board/file/FileService.java
@@ -1,6 +1,7 @@
 package com.animal.mypet.board.file;
 
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @Service("boardFileService")
 public class FileService {
 
+	@Autowired
     @Value("${file.upload-dir}")
     private String fileUploadDir;
 
