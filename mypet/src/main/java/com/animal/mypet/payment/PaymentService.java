e3f6cad21a0b3696f63362b0d566b694d9670f8b
tjdgu <tjdgus5099@naver.com>
2024-12-15 12:46:10 +0900

    수정

--- mypet/src/main/java/com/animal/mypet/payment/PaymentService.java
+++ mypet/src/main/java/com/animal/mypet/payment/PaymentService.java
@@ -13,11 +13,11 @@ import org.springframework.http.ResponseEntity;
 @Service
 public class PaymentService {
 
-	@Value("${IAMPORT_API_KEY}")
-	private String apiKey;
+    @Value("${iamport.api.key}")
+    private String apiKey;
 
-	@Value("${IAMPORT_API_SECRET}")
-	private String apiSecret;
+    @Value("${iamport.api.secret}")
+    private String apiSecret;
 
     private final String API_URL = "https://api.iamport.kr";
     private final ObjectMapper objectMapper = new ObjectMapper();
